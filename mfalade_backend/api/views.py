618b240ba26264e8847dcc28a380e445db4492eb
Mayowa Falade <mayowa.falade@ithaka.org>
2016-12-20 18:23:17 +0100

    Remove unneeded dependency

--- mfalade_backend/api/views.py
+++ mfalade_backend/api/views.py
@@ -1,6 +1,5 @@
 """Views to handle all api requests."""
 import json
-import requests
 
 from flask import jsonify, make_response, request
 from flask_cors import CORS
@@ -23,11 +22,9 @@ def healthcheck():
 
 @api.route('/sendmail', methods=['POST'])
 def sendmail():
-    """View to handle sending of mails."""
     dispatcher.send_email(request.json)
     return json.dumps({
         'status_code': 200,
-        'status': 'sent',
-        'msg': "I see you",
+        'status': 'success',
         'data': request.json
     })
