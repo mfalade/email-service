f93f000ad739ff355b4ecad7bb554804c2cd7f36
Mayowa Falade <mayowa.falade@ithaka.org>
2016-12-20 17:32:37 +0100

    move email template to separate file for easy customizing

--- mfalade_backend/dispatcher.py
+++ mfalade_backend/dispatcher.py
@@ -1,25 +1,15 @@
 from flask_mail import Message
 
 from mfalade_backend.extensions import mail
+from mfalade_backend.templates import base_template
 
 
 def send_email(email_context):
 	msg = Message(
 		"Hey, you've got a new visitor",
-		sender="falademayowa240@gmail.com",
+		sender="notifier@mayowafalade.com",
 		recipients=["falademayowa240@yahoo.com"]
 	)
-	msg_template = """
-		<p>You've got a new message from <b>{messageSender}</b></p>
-		<br />
-		<b>{messageTitle}</b>
-		<hr />
-		<p>Message Body:</p>
-		<pre>{messageBody}</pre>
-		<br />
-		<p>Sender's Contact: {senderContact}</p>
-	"""
-	msg.body = "testing"
-	msg.html = msg_template.format(**email_context)
+	msg.html = base_template.format(**email_context)
 
 	mail.send(msg)
---
+++ mfalade_backend/templates.py
@@ -0,0 +1,10 @@
+base_template =  """
+		<p>You've got a new message from <b>{messageSender}</b></p>
+		<br />
+		<b>{messageTitle}</b>
+		<hr />
+		<p>Message Body:</p>
+		<pre>{messageBody}</pre>
+		<br />
+		<p>Sender's Contact: <b>{senderContact}</b></p>
+	"""
