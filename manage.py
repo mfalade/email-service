2261ecaaf9ef521333220e958344f2e778d5d107
Mayowa Falade <mayowa.falade@ithaka.org>
2016-12-20 18:44:30 +0100

    Update manage command

--- manage.py
+++ manage.py
@@ -1,17 +1,15 @@
-"""."""
 import os
 
-from flask_script import Manager
+from flask_script import Server, Manager
 
 from mfalade_backend import create_app
 
 
 app_env = os.environ.get('MFALADE_ENV', 'dev')
-
 app = create_app(app_env)
 
 manager = Manager(app)
-
+manager.add_command('runserver', Server(host='0.0.0.0', port='3000'))
 
 
 if __name__ == '__main__':
