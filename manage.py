3e41ef5a74027f7a1c001790d6b3785be1766146
Mayowa Falade <mayowa.falade@ithaka.org>
2016-12-20 18:49:58 +0100

    I surrender heroku

--- manage.py
+++ manage.py
@@ -7,9 +7,10 @@ from mfalade_backend import create_app
 
 app_env = os.environ.get('MFALADE_ENV', 'dev')
 app = create_app(app_env)
+port = os.environ.get('PORT', '3000')
 
 manager = Manager(app)
-manager.add_command('runserver', Server(host='0.0.0.0', port='3000'))
+manager.add_command('runserver', Server(host='0.0.0.0', port=port))
 
 
 if __name__ == '__main__':
